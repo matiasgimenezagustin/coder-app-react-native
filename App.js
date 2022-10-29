e7af555b22c7408a43581798ef3d4fa85b81eb01
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-10-29 19:10:33 -0300

    desafio 1, hello world

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hola, Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+const mainColors = {
+  black: "#181818",
+  white: "white"
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: mainColors.black,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: mainColors.white,
+    fontSize: "2.5rem",
+  }
 });
